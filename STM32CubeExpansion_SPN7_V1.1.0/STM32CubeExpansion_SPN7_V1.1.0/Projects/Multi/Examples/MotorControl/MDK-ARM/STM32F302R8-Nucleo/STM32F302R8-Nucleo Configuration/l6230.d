b750aa906f98ef34f26155e8f17df5b331889c5e
Butaforsky <gostev.ifmo@gmail.com>
2020-11-18 02:35:39 +0300

    New TIM and CHxN
    Added TIMEx_Start/Stop in X-NUCLEO-IHM07M1.c
    Added advanced complementary channel support

---
+++ STM32CubeExpansion_SPN7_V1.1.0/STM32CubeExpansion_SPN7_V1.1.0/Projects/Multi/Examples/MotorControl/MDK-ARM/STM32F302R8-Nucleo/STM32F302R8-Nucleo Configuration/l6230.d
@@ -0,0 +1,4 @@
+stm32f302r8-nucleo configuration\l6230.o: ..\..\..\..\..\..\Drivers\BSP\Components\l6230\l6230.c
+stm32f302r8-nucleo configuration\l6230.o: ..\..\..\..\..\..\Drivers\BSP\Components\l6230\l6230.h
+stm32f302r8-nucleo configuration\l6230.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32f302r8-nucleo configuration\l6230.o: ..\..\..\..\..\..\Drivers\BSP\Components\Common\MC_Common.h
