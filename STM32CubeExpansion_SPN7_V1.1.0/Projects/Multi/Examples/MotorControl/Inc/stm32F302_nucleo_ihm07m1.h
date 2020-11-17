3d675277a1d0e3551cc8452b6020f62b57c534a5
Marcusfur <69159531+Marcusfur@users.noreply.github.com>
2020-11-17 15:35:01 +0300

    Update stm32F302_nucleo_ihm07m1.h
    Added info about further port usage

--- STM32CubeExpansion_SPN7_V1.1.0/Projects/Multi/Examples/MotorControl/Inc/stm32F302_nucleo_ihm07m1.h
+++ STM32CubeExpansion_SPN7_V1.1.0/Projects/Multi/Examples/MotorControl/Inc/stm32F302_nucleo_ihm07m1.h
@@ -49,29 +49,33 @@
   #define HALL_ENCODER_TIMx     htim2
   #define ADCx                  hadc1
   #define REFx                  htim16
-  #define UART                  huart2
+  #define UART                  huart2          //NO NEED IN THIS PORT, USE huart3 instead
 
-  #define GPIO_PORT_1           GPIOC
-  #define GPIO_CH1              GPIO_PIN_10
-  #define GPIO_PORT_2           GPIOC
-  #define GPIO_CH2              GPIO_PIN_11
-  #define GPIO_PORT_3           GPIOC
-  #define GPIO_CH3              GPIO_PIN_12
+  #define GPIO_PORT_1           GPIOC           //NO NEED IN THIS PORT, Used as enable of PWM channels, Remove after complimentaru PWM implementation
+  #define GPIO_CH1              GPIO_PIN_10     //same
+  #define GPIO_PORT_2           GPIOC           //NO NEED IN THIS PORT, Used as enable of PWM channels, Remove after complimentaru PWM implementation
+  #define GPIO_CH2              GPIO_PIN_11     //same    
+  #define GPIO_PORT_3           GPIOC           //NO NEED IN THIS PORT, Used as enable of PWM channels, Remove after complimentaru PWM implementation
+  #define GPIO_CH3              GPIO_PIN_12     //same
   #define GPIO_SET              GPIO_PIN_SET
   #define GPIO_RESET            GPIO_PIN_RESET
 
   #define ADC_CH_1              ADC_CHANNEL_7    /*CURRENT*/
-  #define ADC_CH_2              ADC_CHANNEL_12   /*SPEED*/
-  #define ADC_CH_3              ADC_CHANNEL_2    /*VBUS*/
-  #define ADC_CH_4              ADC_CHANNEL_8    /*TEMP*/
+//#define ADC_CH_1              ADC_CHANNEL_2    /*Phase current U*/
+//#define ADC_CH_2              ADC_CHANNEL_3    /*Phase current V*/
+//#define ADC_CH_2              ADC_CHANNEL_4    /*Phase current W*/
+
+  #define ADC_CH_2              ADC_CHANNEL_12   /*SPEED*/  //I dunno wft is this, maybe remove
+  #define ADC_CH_3              ADC_CHANNEL_2    /*VBUS*/   //NO NEED IN THIS MEASUREMENT! REMOVE
+  #define ADC_CH_4              ADC_CHANNEL_8    /*TEMP*/   //NO NEED IN THIS MEASUREMENT! REMOVE
   #define ADC_Bemf_CH1          ADC_CHANNEL_9    /*BEMF1*/
   #define ADC_Bemf_CH2          ADC_CHANNEL_11   /*BEMF2*/
   #define ADC_Bemf_CH3          ADC_CHANNEL_15   /*BEMF3*/
 
   #define ADC_CH_1_ST           ADC_SAMPLETIME_1CYCLE_5    /*CURRENT sampling time */
   #define ADC_CH_2_ST           ADC_SAMPLETIME_181CYCLES_5 /*SPEED sampling time*/
-  #define ADC_CH_3_ST           ADC_SAMPLETIME_181CYCLES_5 /*VBUS sampling time*/
-  #define ADC_CH_4_ST           ADC_SAMPLETIME_181CYCLES_5 /*TEMP sampling time*/
+  #define ADC_CH_3_ST           ADC_SAMPLETIME_181CYCLES_5 /*VBUS sampling time*/  /*REMOVE
+  #define ADC_CH_4_ST           ADC_SAMPLETIME_181CYCLES_5 /*TEMP sampling time*/  /*REMOVE
   #define ADC_Bemf_CH1_ST       ADC_SAMPLETIME_61CYCLES_5  /*BEMF1 sampling time*/
   #define ADC_Bemf_CH2_ST       ADC_SAMPLETIME_61CYCLES_5  /*BEMF2 sampling time*/
   #define ADC_Bemf_CH3_ST       ADC_SAMPLETIME_61CYCLES_5  /*BEMF3 sampling time*/
@@ -90,6 +94,8 @@
 
   #define GPIO_PORT_ZCR         GPIOC           /*!<  GPIO port name for zero crossing detection */
   #define GPIO_CH_ZCR           GPIO_PIN_7      /*!<  GPIO pin name for zero crossing detection */
+ // #define GPIO_PORT_ZCR         GPIOC         /*!<  EDITED _ GPIO port name for zero crossing detection */
+ // #define GPIO_CH_ZCR           GPIO_PIN_9    /*!<  EDITED _ GPIO pin name for zero crossing detection */
   #define GPIO_PORT_COMM        GPIOC           /*!<  GPIO port name for 6Step commutation */
   #define GPIO_CH_COMM          GPIO_PIN_4      /*!<  GPIO pin name for 6Step commutation */
 
