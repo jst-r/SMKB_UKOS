2042dd63e5c116b0a4ea3b3ae4a7495c23ef678e
Butaforsky <gostev.ifmo@gmail.com>
2020-11-23 16:09:26 +0300

    PCB-prototype changed work sequence of echo-handler

--- PCB-UART-Prototype/MDK-ARM/PCB-UART-Prototype/main.d
+++ PCB-UART-Prototype/MDK-ARM/PCB-UART-Prototype/main.d
@@ -7,13 +7,13 @@ pcb-uart-prototype\main.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def
 pcb-uart-prototype\main.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h
 pcb-uart-prototype\main.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302x8.h
 pcb-uart-prototype\main.o: ../Drivers/CMSIS/Include/core_cm4.h
-pcb-uart-prototype\main.o: D:\system\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+pcb-uart-prototype\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 pcb-uart-prototype\main.o: ../Drivers/CMSIS/Include/cmsis_version.h
 pcb-uart-prototype\main.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 pcb-uart-prototype\main.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
 pcb-uart-prototype\main.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h
 pcb-uart-prototype\main.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h
-pcb-uart-prototype\main.o: D:\system\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+pcb-uart-prototype\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
 pcb-uart-prototype\main.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h
 pcb-uart-prototype\main.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h
 pcb-uart-prototype\main.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h
