8698ac7c64506f8419d3e2aa5d1dbff2904468ce
jst-r <jstr271@gmail.com>
2021-02-18 16:58:50 +0300

    seqAnalysis fixes

--- OneWire_DS/Core/Inc/seqAnalysis.h
+++ OneWire_DS/Core/Inc/seqAnalysis.h
@@ -1,15 +1,17 @@
 #ifndef SEQ_ANALYSIS_H
 #define SEQ_ANALYSIS_H
 
+#include "main.h"
+
 #define buff_size 500
 #define time_window 100  // ms
 #define sample_time 1    // ms
 
 #define n_delays 5
 
-const unsigned char delays[] = {1, 2, 3, 4};
-unsigned char buff[buff_size];
-unsigned char mask[buff_size];
+const uint8_t delays[] = {1, 2, 3, 4};
+uint8_t buff[buff_size];
+uint8_t mask[buff_size];
 
 void listen();
 void init_mask();
